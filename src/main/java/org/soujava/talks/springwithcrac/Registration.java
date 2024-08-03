3add07caf4f499d678a82a1aebbcafd20dd9fb7d
Maximillian Arruda <dearrudam@gmail.com>
2024-08-03 05:44:34 -0300

    chore: solving N+1 issue

--- src/main/java/org/soujava/talks/springwithcrac/Registration.java
+++ src/main/java/org/soujava/talks/springwithcrac/Registration.java
@@ -1,5 +1,6 @@
 package org.soujava.talks.springwithcrac;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,11 @@ import java.util.List;
 @Repository
 interface Registration extends ListCrudRepository<Attendee, Long> {
 
+    @EntityGraph(
+            attributePaths = {
+                    "technologies"
+            }
+    )
     List<Attendee> findAll();
 
 }
